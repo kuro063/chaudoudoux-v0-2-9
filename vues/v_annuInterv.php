e879f63bae68e27278a605423bcff91e67d63e07
Owen SOK <johnbaclaouie97@gmail.com>
2025-05-30 17:56:51 +0200

    Changement organisation code pour plus de visibilité

--- vues/v_annuInterv.php
+++ vues/v_annuInterv.php
@@ -215,88 +215,131 @@
         }}}
         echo '<span style="font-size: 25px" ><strong>- PRESTATION MÉNAGE</strong> - Nombre d\'heures par semaine : '.round($nbhTot,2).' h</span>';
         ?><tfoot><tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td><strong style="float:right"><?php echo 'Total Prestataire : '.round($nbhTot,2); ?></tfoot></strong></td></tr></tfoot></table><br/><br/>
-                <table class="sortable zebre" id="listeIntervGardeEnfant">
-        <thead>   <tr class="btn-secondary">
-            <?php if (lireDonneeUrl('num')!=99999){?><th style="display: none;">addr_fam</th><th style="display: none;">addr_can</th><th>Numéro du salarié</th><th>Nom du Salarié</th><th>Nom de jeune fille</th><?php }?><th>Nom de la famille</th><th>Type de prestation</th><th>Date de début</th><th>Date de fin</th><th>Jour / horaires / modalités</th><th>HRES/SEM</th><th>distance Interv-Famille</th><th>distance Chaudoudoux-Famille</th><th>Modifier</th><th>Terminé?</th>
-            </tr></thead><tbody><?php $i=0;$nbhTot=0;
-         foreach ($lesInterv as $uneInterv)
-        {$numFam= $uneInterv['numero_Famille'];
-            if ($numFam!=9999 && $uneInterv['type_Prestations'] == 'Garde d\'enfants'){
-            $numSal= $uneInterv['numSalarie_Intervenants'];
-            $nomSal=$uneInterv['nom_Candidats'];
-            $nomJF=$uneInterv['nomJF_Candidats'];
-            $infoCan=$pdoChaudoudoux->obtenirDetailsSalarie($numSal);
-            $addrCan = 
-            (!isset($infoCan[0]['adresse_Candidats']) || is_bool($infoCan[0]['adresse_Candidats']) ? '' : $infoCan[0]['adresse_Candidats']) . ' ' . 
-            (!isset($infoCan[0]['cp_Candidats']) || is_bool($infoCan[0]['cp_Candidats']) ? '' : $infoCan[0]['cp_Candidats']) . ' ' . 
-            (!isset($infoCan[0]['ville_Candidats']) || is_bool($infoCan[0]['ville_Candidats']) ? '' : $infoCan[0]['ville_Candidats']);
-            
-            
-            
-            $infoFam=$pdoChaudoudoux->obtenirDetailFamille($numFam);
-            
-            $addrFam = 
-            (!isset($infoFam['adresse_Famille']) || is_bool($infoFam['adresse_Famille']) ? '' : $infoFam['adresse_Famille']) . ' ' . 
-            (!isset($infoFam['cp_Famille']) || is_bool($infoFam['cp_Famille']) ? '' : $infoFam['cp_Famille']) . ' ' . 
-            (!isset($infoFam['ville_Famille']) || is_bool($infoFam['ville_Famille']) ? '' : $infoFam['ville_Famille']);
+        <table class="sortable zebre" id="listeIntervGardeEnfant">
+        <thead>   
+          <tr class="btn-secondary">
+            <?php if (lireDonneeUrl('num')!=99999){?>
+              <th style="display: none;">addr_fam</th>
+              <th style="display: none;">addr_can</th>
+              <th>Numéro du salarié</th>
+              <th>Nom du Salarié</th>
+              <th>Nom de jeune fille</th>
+            <?php }?>
+              <th>Nom de la famille</th>
+              <th>Type de prestation</th>
+              <th>Date de début</th>
+              <th>Date de fin</th>
+              <th>Jour / horaires / modalités</th>
+              <th>HRES/SEM</th>
+              <th>distance Interv-Famille</th>
+              <th>distance Chaudoudoux-Famille</th>
+              <th>Modifier</th>
+              <th>Terminé?</th>
+            </tr>
+        </thead>
+        <tbody>
+          <?php $i=0;
+            $nbhTot=0;
+            foreach ($lesInterv as $uneInterv){
+              $numFam= $uneInterv['numero_Famille'];
 
+              if ($numFam!=9999 && $uneInterv['type_Prestations'] == 'Garde d\'enfants'){
+                $numSal= $uneInterv['numSalarie_Intervenants'];
+                $nomSal=$uneInterv['nom_Candidats'];
+                $nomJF=$uneInterv['nomJF_Candidats'];
+                $infoCan=$pdoChaudoudoux->obtenirDetailsSalarie($numSal);
 
+                $addrCan = 
+                (!isset($infoCan[0]['adresse_Candidats']) || is_bool($infoCan[0]['adresse_Candidats']) ? '' : $infoCan[0]['adresse_Candidats']) . ' ' . 
+                (!isset($infoCan[0]['cp_Candidats']) || is_bool($infoCan[0]['cp_Candidats']) ? '' : $infoCan[0]['cp_Candidats']) . ' ' . 
+                (!isset($infoCan[0]['ville_Candidats']) || is_bool($infoCan[0]['ville_Candidats']) ? '' : $infoCan[0]['ville_Candidats']);
+                
+                $infoFam=$pdoChaudoudoux->obtenirDetailFamille($numFam);
+                
+                $addrFam = 
+                (!isset($infoFam['adresse_Famille']) || is_bool($infoFam['adresse_Famille']) ? '' : $infoFam['adresse_Famille']) . ' ' . 
+                (!isset($infoFam['cp_Famille']) || is_bool($infoFam['cp_Famille']) ? '' : $infoFam['cp_Famille']) . ' ' . 
+                (!isset($infoFam['ville_Famille']) || is_bool($infoFam['ville_Famille']) ? '' : $infoFam['ville_Famille']);
 
+                $nomFam=$pdoChaudoudoux->obtenirNomFamille($numFam);
+                $idSal= $uneInterv['idSalarie_Intervenants'];
+                $options=$uneInterv['options_Proposer'];
+                $presta=$uneInterv['type_Prestations'];
+                $dateDeb=$uneInterv['dateDeb_Proposer'];
+                $freq=$uneInterv['frequence_Proposer'];
+                $dateFin=$uneInterv['dateFin_Proposer'];
+                $idADH=$uneInterv['idADH_TypeADH'];
+                $idPresta=$uneInterv['idPresta_Prestations'];
+                $hDeb=$uneInterv['hDeb_Proposer'];
+                $hFin= $uneInterv['hFin_Proposer'];
+                $jour= $uneInterv['jour_Proposer'];
+                $modalites=$uneInterv['modalites_Proposer'];
 
-            $nomFam=$pdoChaudoudoux->obtenirNomFamille($numFam);
-            $idSal= $uneInterv['idSalarie_Intervenants'];
-            $options=$uneInterv['options_Proposer'];
-            $presta=$uneInterv['type_Prestations'];
-            $dateDeb=$uneInterv['dateDeb_Proposer'];
-            $freq=$uneInterv['frequence_Proposer'];
-            $dateFin=$uneInterv['dateFin_Proposer'];
-            $idADH=$uneInterv['idADH_TypeADH'];
-            $idPresta=$uneInterv['idPresta_Prestations'];
-            $hDeb=$uneInterv['hDeb_Proposer'];
-            $hFin= $uneInterv['hFin_Proposer'];
-            $jour= $uneInterv['jour_Proposer'];
-            $modalites=$uneInterv['modalites_Proposer'];
-            if ($dateFin=='0000-00-00'){$dateFin="";}
-            $dateDebstring="";
-            $dateFinstring="";
-            if($dateDeb!=""){
-            $dateDebstring= dateToString($uneInterv['dateDeb_Proposer']);}
-            if ($dateFin!=""){
-            $dateFinstring=dateToString($uneInterv['dateFin_Proposer']);}
-            if ($dateFinstring=='00/00/0000'){$dateFinstring="Indeterminé";}
-            $statut=$uneInterv['Statut_Proposer'];
-            $validInterv=$uneInterv['validInterv_Proposer'];
-            if ($validInterv==1) {$validInterv='OUI';} elseif ($validInterv==0){$validInterv='NON';} else {$validInterv='NC';}
-            $validFam=$uneInterv['validFamille_Proposer'];
-            if ($validFam==1) {$validFam='OUI';} elseif ($validFam==0){$validFam='NON';} else {$validFam='NC';}
-            $libelleADH=$uneInterv['intitule_TypeADH'];
-            if ($idADH!='MAND'){
-            echo '<tr>';
-            echo '<td style="display: none;">'.$addrCan.'</td><td style="display: none;">'.$addrFam.'</td>';
-            if (lireDonneeUrl('num')!=99999){ echo '<td>'.$idSal.'</td><td><a href="index.php?uc=annuSalarie&action=voirDetailSalarie&num='.$numSal.'">'.$nomSal.'</a></td><td>'.$nomJF.'</td>';} echo '<td><a href="index.php?uc=annuFamille&action=voirDetailFamille&num='.$numFam.'">'.$nomFam.'</td><td>';
-            echo $presta." ".$libelleADH;
-            echo'</td><td>'.$dateDebstring.'</td><td>'.$dateFinstring.'</td><td sorttable_customkey="'.ckJour($jour).$hDeb.$hFin.'">';
-            echo $jour.' '.$hDeb.'-'.$hFin.'<br/>'.$modalites.'<td>';
-            $nbminFin=((int) substr($hFin,3,2));$nbminDeb=((int) substr($hDeb, 3,2));
-            if ($nbminFin==45){$nbminFin=0.75;}elseif($nbminFin==30){$nbminFin=0.5;}elseif($nbminFin==15){$nbminFin=0.25;}else{$nbminFin=0;}
-            if ($nbminDeb==45){$nbminDeb=0.75;}elseif($nbminDeb==30){$nbminDeb=0.5;}elseif($nbminDeb==15){$nbminDeb=0.25;}else{$nbminDeb=0;}
+                if ($dateFin=='0000-00-00'){
+                  $dateFin="";}
 
-            $nbh=(((int) substr($hFin, 0,2))+$nbminFin-(((int)substr($hDeb,0,2))+$nbminDeb))/$freq;
-            echo $nbh.' heures';
-            $nbhTot+=$nbh; 
-            
-            $hDeburl= substr($hDeb,0,2).substr($hDeb,3,2).substr($hDeb,6,2);
-            $hFinurl= substr($hFin,0,2).substr($hFin,3,2).substr($hFin,6,2);
-            $dateDeburl= substr($dateDeb, 0,4).substr($dateDeb,5,2).substr($dateDeb, 8,2);
-            $dateFinurl=  substr($dateFin, 0,4).substr($dateFin,5,2).substr($dateFin, 8,2);
+                $dateDebstring="";
+                $dateFinstring="";
+                
+                if($dateDeb!=""){
+                $dateDebstring= dateToString($uneInterv['dateDeb_Proposer']);}
 
-            echo '</td><td style="border-left: solid black 1px;border-right: solid black 1px; border-collapse : collapse"><img style="max-width:30px;" src="./styles/img/loading.gif"></img></td>';
-            echo '</td><td style="border-left: solid black 1px;border-right: solid black 1px; border-collapse : collapse"><img style="max-width:30px;" src="./styles/img/loading.gif"></img></td>';
+                if ($dateFin!=""){
+                $dateFinstring=dateToString($uneInterv['dateFin_Proposer']);}
 
-            echo '</td><td><a href="index.php?uc=interventions&amp;action=modifInterv&amp;num='.$numSal.'&amp;numFam='.$numFam.'&amp;idADH='.$idADH.'&amp;idPresta='.$idPresta.'&amp;hDeb='.$hDeburl.'&amp;dateDeb='.$dateDeburl.'&amp;dateFin='.$dateFinurl.'&amp;jour='.$jour.'&amp;hFin='.$hFinurl.'&amp;freq='.$freq.'">Modifier</a>';
-            echo '</td><td><a href="index.php?uc=interventions&amp;action=archiverIntervention&amp;num='.$numSal.'&amp;numFam='.$numFam.'&amp;idADH='.$idADH.'&amp;idPresta='.$idPresta.'&amp;hDeb='.$hDeburl.'&amp;dateDeb='.$dateDeburl.'&amp;jour='.$jour.'&amp;hFin='.$hFinurl.'">Terminé</a></td></tr>';
-            $i++;
-        }}}
+                if ($dateFinstring=='00/00/0000'){
+                $dateFinstring="Indeterminé";}
+                
+                $statut=$uneInterv['Statut_Proposer'];
+                $validInterv=$uneInterv['validInterv_Proposer'];
+
+                if ($validInterv==1) {$validInterv='OUI';} elseif ($validInterv==0){$validInterv='NON';} else {$validInterv='NC';}
+                $validFam=$uneInterv['validFamille_Proposer'];
+
+                if ($validFam==1) {$validFam='OUI';} elseif ($validFam==0){$validFam='NON';} else {$validFam='NC';}
+                $libelleADH=$uneInterv['intitule_TypeADH'];
+
+                if ($idADH!='MAND'){
+                  echo '<tr>';
+                    echo '<td style="display: none;">'.$addrCan.'</td>
+                          <td style="display: none;">'.$addrFam.'</td>';
+                  if (lireDonneeUrl('num')!=99999){ 
+                    echo '<td>'.$idSal.'</td>
+                          <td><a href="index.php?uc=annuSalarie&action=voirDetailSalarie&num='.$numSal.'">'.$nomSal.'</a></td>
+                          <td>'.$nomJF.'</td>';} 
+                    echo '<td><a href="index.php?uc=annuFamille&action=voirDetailFamille&num='.$numFam.'">'.$nomFam.'</td>
+                          <td>';
+                    echo $presta." ".$libelleADH;
+                    echo'</td>
+                          <td>'.$dateDebstring.'</td>
+                          <td>'.$dateFinstring.'</td>
+                          <td sorttable_customkey="'.ckJour($jour).$hDeb.$hFin.'">';
+                  echo $jour.' '.$hDeb.'-'.$hFin.'<br/>'.$modalites.'<td>';
+                  $nbminFin=((int) substr($hFin,3,2));$nbminDeb=((int) substr($hDeb, 3,2));
+                  if ($nbminFin==45){$nbminFin=0.75;}elseif($nbminFin==30){$nbminFin=0.5;}elseif($nbminFin==15){$nbminFin=0.25;}else{$nbminFin=0;}
+                  if ($nbminDeb==45){$nbminDeb=0.75;}elseif($nbminDeb==30){$nbminDeb=0.5;}elseif($nbminDeb==15){$nbminDeb=0.25;}else{$nbminDeb=0;}
+
+                  $nbh=(((int) substr($hFin, 0,2))+$nbminFin-(((int)substr($hDeb,0,2))+$nbminDeb))/$freq;
+                  echo $nbh.' heures';
+                  $nbhTot+=$nbh; 
+                  
+                  $hDeburl= substr($hDeb,0,2).substr($hDeb,3,2).substr($hDeb,6,2);
+                  $hFinurl= substr($hFin,0,2).substr($hFin,3,2).substr($hFin,6,2);
+                  $dateDeburl= substr($dateDeb, 0,4).substr($dateDeb,5,2).substr($dateDeb, 8,2);
+                  $dateFinurl=  substr($dateFin, 0,4).substr($dateFin,5,2).substr($dateFin, 8,2);
+
+                  echo '</td>
+                        <td style="border-left: solid black 1px;border-right: solid black 1px; border-collapse : collapse"><img style="max-width:30px;" src="./styles/img/loading.gif"></img></td>';
+                  echo '</td>
+                        <td style="border-left: solid black 1px;border-right: solid black 1px; border-collapse : collapse"><img style="max-width:30px;" src="./styles/img/loading.gif"></img></td>';
+                  echo '</td>
+                        <td><a href="index.php?uc=interventions&amp;action=modifInterv&amp;num='.$numSal.'&amp;numFam='.$numFam.'&amp;idADH='.$idADH.'&amp;idPresta='.$idPresta.'&amp;hDeb='.$hDeburl.'&amp;dateDeb='.$dateDeburl.'&amp;dateFin='.$dateFinurl.'&amp;jour='.$jour.'&amp;hFin='.$hFinurl.'&amp;freq='.$freq.'">Modifier</a>';
+                  echo '</td>
+                        <td><a href="index.php?uc=interventions&amp;action=archiverIntervention&amp;num='.$numSal.'&amp;numFam='.$numFam.'&amp;idADH='.$idADH.'&amp;idPresta='.$idPresta.'&amp;hDeb='.$hDeburl.'&amp;dateDeb='.$dateDeburl.'&amp;jour='.$jour.'&amp;hFin='.$hFinurl.'">Terminé</a></td></tr>';
+                  $i++;
+                }
+              }
+            }
         echo '<span style="font-size: 25px" ><strong>- PRESTATION GARDE D\'ENFANTS UNIQUEMENT EN PERIODE SCOLAIRE</strong> - Nombre d\'heures par semaine : '.round($nbhTot,2).' h</span>';
         ?><tfoot><tr><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td><td><strong style="float:right"><?php echo 'Total Prestataire : '.round($nbhTot,2); ?></tfoot></strong></td></tr></tfoot></table><br/><br/>
                 <table class="sortable zebre" id="listeInterv">
@@ -403,9 +446,22 @@
         <h2>Historique des interventions</h2>
                
       <table class="sortable zebre" id="listeInterv">
-          <thead> <tr class="btn-secondary">
-            <?php if (lireDonneeUrl('num')!=99999){?><th>Numéro du salarié</th><th>Nom du Salarié</th><th>Nom de jeune fille</th><?php }?><th>Nom de la famille</th><th>Type de prestation</th><th>Date de début</th><th>Date de fin</th><th>Jour / horaires / modalités</th><th>Modifier</th>
-              </tr></thead><tbody><?php $i=0;
+          <thead> 
+            <tr class="btn-secondary">
+            <?php if (lireDonneeUrl('num')!=99999){?>
+              <th>Numéro du salarié</th>
+              <th>Nom du Salarié</th>
+              <th>Nom de jeune fille</th><?php }?>
+              <th>Nom de la famille</th>
+              <th>Type de prestation</th>
+              <th>Date de début</th>
+              <th>Date de fin</th>
+              <th>Jour / horaires / modalités</th>
+              <th>Modifier</th>
+              </tr>
+            </thead>
+          <tbody>
+          <?php $i=0;
         foreach ($lesIntervPasse as $uneIntervPasse)
         { $numFam= $uneIntervPasse['numero_Famille'];
             if ($numFam!=9999){
